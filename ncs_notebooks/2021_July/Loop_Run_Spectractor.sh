c5f43190fc96b8d9586d0537ec68f94b6c775ec0
Dagoret Campagne Sylvie <dagoret@lal.in2p3.fr>
2021-09-14 17:18:47 +0200

    correct a bug

--- ncs_notebooks/2021_July/Loop_Run_Spectractor.sh
+++ ncs_notebooks/2021_July/Loop_Run_Spectractor.sh
@@ -11,13 +11,19 @@ eups list -s | grep LOCAL
 # narrow scan :  HD 160617 
 # all_myseq_holo=range(317,365)
 
-
-date ="2921-07-07"
 path_output_dir=/project/shared/auxTel/rerun/dagoret/outputspectr_scan2021_July/spectractorOutput/
+
+date ="2021-07-07"
+
 full_path_output_dir="$path_output_dir $date"
 
+# wide scan
 #EXPNUMS=`seq 234 1 309`
-EXPNUMS=`seq 244 1 309`
+
+# narrow scan
+#EXPNUMS=`seq 317 1 365`
+EXPNUMS=`seq 318 1 365`
+
 LISTNUM=''
 
 # loop on exposures
@@ -26,11 +32,12 @@ do
 LISTNUM="${LISTNUM} $num"
 echo " num = $num"
 #output=$(python Run_Spectractor_example_2021-07-07.py -n $num 2>&1) 
-output=$(python Run_Spectractor_example.py -n $num -d $date 2>&1) 
+cmd="python Run_Spectractor_example.py -n $num -d $date 2>&1"
+echo $cmd
+output=$(cmd) 
 echo $output
 done
 
 echo "List of Exposures : " $LISTNUM
 
 
-ls -l $full_path_output_dir
