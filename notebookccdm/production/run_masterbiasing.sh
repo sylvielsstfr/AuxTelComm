dda82d58d2a940f0025926ec342861530a0c1df1
Dagoret Campagne Sylvie <dagoret@lal.in2p3.fr>
2022-01-26 15:02:26 +0100

    update master biasing

--- notebookccdm/production/run_masterbiasing.sh
+++ notebookccdm/production/run_masterbiasing.sh
@@ -5,15 +5,15 @@
 #source /cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/w_2021_44/loadLSST.bash
 #setup lsst_distrib
 
-#PATH=dirname "$@"
-#echo "PATH=$PATH"
+
+RERUNDIR=dagoret_fordispersers2022_01  
  
 for file in "$@"
 do
   echo "Processing $file file..."
   # take action on each file. $file store current file name
   cat "$file"
-  cmd="constructBias.py . --rerun dagoret_jan22_bias  @${file}  --batch-type none --configfile biasConfig.py --clobber-config"
+  cmd="constructBias.py . --rerun ${RERUNDIR}  @${file}  --batch-type none --configfile biasConfig.py --clobber-config"
   echo -e "${cmd}"
   `$cmd`
  
