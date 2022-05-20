c7b521ae78a04b8fcbd8b9c44c5f9c6059c660d2
Dagoret Campagne Sylvie <dagoret@lal.in2p3.fr>
2022-05-20 17:47:28 +0200

    update astrometry

--- notebookccdm_gen3/astrometry/astrom_eta01dor_march2022.sh
+++ notebookccdm_gen3/astrometry/astrom_eta01dor_march2022.sh
@@ -7,9 +7,9 @@ ra=91.53909041666665
 dec=-66.03962083333333
 
 path="/sps/lsst/groups/auxtel/data/2022/empty~ronchi170lpmm/20220316"
-image="exposure_2022031600334_postisrccd.fits"
+image="exposure_2022031600335_postisrccd.fits"
 
-cmd="solve-field --scale-units arcsecperpix --scale-low 0.01 --scale-high 0.1 --ra $ra --dec $dec --radius 0.076 --overwrite --ext 1 $path/$image"
+cmd="solve-field --scale-units arcsecperpix --scale-low 0.01 --scale-high 0.1 --ra $ra --dec $dec --radius 0.076 --overwrite --ext 1 -D . $path/$image"
 
 echo $cmd
 
