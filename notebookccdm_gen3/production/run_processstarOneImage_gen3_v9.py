231c2e0f6056f6b77e70f2730dfc718619a3efb5
Dagoret Campagne Sylvie <dagoret@lal.in2p3.fr>
2022-06-29 16:08:46 +0200

    add source run_processstarOneImage_gen3_v9.py

---
+++ notebookccdm_gen3/production/run_processstarOneImage_gen3_v9.py
@@ -0,0 +1,56 @@
+#
+# run_processstarOneImage_gen3_v9.py --reposdir repos/w_2022_09 --listofimages all_visitsdispersers_gen3/visitdispersers_20220317_filt_empty-ronchi170lpmm.list --num=3
+#
+
+import os
+import subprocess
+import numpy as np
+import argparse
+
+
+reposdir="repos/w_2022_09"
+listofimages="all_visitsdispersers_gen3/visitdispersers_20220317_filt_empty-ronchi170lpmm.list"
+
+
+# Create the parser
+parser = argparse.ArgumentParser()# Add an argument
+parser.add_argument('--reposdir', type=str, required=True)
+parser.add_argument('--listofimages', type=str, required=True)
+parser.add_argument('--num', type=int, required=True)
+args = parser.parse_args()
+
+print("args = ",args)
+
+# decode the arguments
+
+
+REPOSDIR=args.reposdir
+listofimages=args.listofimages
+num=args.num
+
+print(REPOSDIR)
+print(listofimages)
+print(num)
+
+arr=np.loadtxt(listofimages,delimiter=' ', dtype =float).astype(int)
+
+dates=arr[:,0]
+seqnum=arr[:,1]
+
+N=len(seqnum)
+
+dateobs=dates[num]
+seq=seqnum[num]
+        
+#cmd1="pipetask run -b ./data -p {}/atmospec/pipelines/processStar.yaml -i LATISS/raw/all,refcats,LATISS/calib -o u/dagoret/first_test2".format(REPOSDIR)
+#print(cmd1)
+   
+#cmd2="-d \"exposure.day_obs={} and exposure.seq_num={} and instrument='LATISS'\" --register-dataset-types --clobber-outputs".format(dateobs,seq)
+#print(cmd2)
+
+command_line="pipetask run -b ./data -p {}/atmospec/pipelines/processStar.yaml -i LATISS/raw/all,refcats,LATISS/calib -o u/dagoret/first_test2 -d \"exposure.day_obs={} and exposure.seq_num={} and instrument='LATISS'\" --register-dataset-types --clobber-outputs".format(REPOSDIR,dateobs,seq)
+
+print(command_line)
+os.system(command_line)
+
+
