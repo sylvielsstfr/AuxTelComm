e0345f43a91f5bf1029e8a72781b833a33719f2b
Sylvie Dagoret-Campagne <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2022-09-20 07:08:08 -0700

    add run_processstar

---
+++ notebooks_usdf/production/README.md
@@ -0,0 +1,10 @@
+# README.md
+
+- author Sylvie Dagoret-Campagne
+- creation date : 2022/09/20 
+
+source  /sdf/group/rubin/sw/tag/w_2022_34/loadLSST.bash
+setup lsst_distrib
+source /sdf/home/d/dagoret/notebooks/.user_setups
+eups list -s | grep LOCAL
+ 
---
+++ notebooks_usdf/production/run_processstarOneImage.py
@@ -0,0 +1,52 @@
+#command line
+#
+# python run_processstarOneImage.py --reposdir /sdf/home/d/dagoret/repos/repos_w_2022_34 --listofimages /sdf/home/d/dagoret/notebooks/AuxTelComm/notebooks_usdf/butlertools/all_visitdispersers/20220607/visitdispersers_20220607_filt_empty-holo4_003.list --num=1
+#
+
+import os
+import subprocess
+import numpy as np
+import argparse
+
+
+reposdir="/sdf/home/d/dagoret/repos/repos_w_2022_34"
+listofimages="/sdf/home/d/dagoret/notebooks/AuxTelComm/notebooks_usdf/butlertools/all_visitdispersers/20220607/visitdispersers_20220607_filt_empty-holo4_003.list"
+
+
+# Create the parser
+parser = argparse.ArgumentParser()# Add an argument
+parser.add_argument('--reposdir', type=str, required=True)
+parser.add_argument('--listofimages', type=str, required=True)
+parser.add_argument('--num', type=int, required=True)
+args = parser.parse_args()
+
+print("args = ",args)
+
+# decode the arguments
+
+
+REPOSDIR=args.reposdir
+listofimages=args.listofimages
+num=args.num
+
+print(REPOSDIR)
+print(listofimages)
+print(num)
+
+arr=np.loadtxt(listofimages,delimiter=' ', dtype =float).astype(int)
+
+dates=arr[:,0]
+seqnum=arr[:,1]
+
+N=len(seqnum)
+
+dateobs=dates[num-1]
+seq=seqnum[num-1]
+        
+
+command_line="pipetask run -b /sdf/group/rubin/repo/main -p {}/atmospec/pipelines/processStar.yaml -i LATISS/raw/all,refcats,LATISS/calib -o u/dagoret/test_auxtel_v0 -d \"exposure.day_obs={} and exposure.seq_num={} and instrument='LATISS'\" --register-dataset-types --clobber-outputs".format(REPOSDIR,dateobs,seq)
+
+print(command_line)
+#os.system(command_line)
+
+
