954cc8f591278db968f5726e690343bf26019374
Sylvie Dagoret-Campagne <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2022-09-22 13:15:56 -0700

    correct run_processstarOneImage.py

--- notebooks_usdf/production/README.md
+++ notebooks_usdf/production/README.md
@@ -1,10 +1,22 @@
 # README.md
 
 - author Sylvie Dagoret-Campagne
-- creation date : 2022/09/20 
+- creation date : 2022/09/20
+- update : 2022/09/22 
+- version w_2022_39
 
-source  /sdf/group/rubin/sw/tag/w_2022_34/loadLSST.bash
+source  /sdf/group/rubin/sw/tag/w_2022_39/loadLSST.bash
 setup lsst_distrib
 source /sdf/home/d/dagoret/notebooks/.user_setups
 eups list -s | grep LOCAL
- 
+
+
+
+# Run ISR
+run_pipeline_ISR.ipynb
+
+# Example of processStar through a notebook
+run_pipeline_processStar.ipynb
+
+# Run from command line
+run_processstarOneImage.py 
--- notebooks_usdf/production/run_processstarOneImage.py
+++ notebooks_usdf/production/run_processstarOneImage.py
@@ -1,6 +1,6 @@
 #command line
 #
-# python run_processstarOneImage.py --reposdir /sdf/home/d/dagoret/repos/repos_w_2022_34 --listofimages /sdf/home/d/dagoret/notebooks/AuxTelComm/notebooks_usdf/butlertools/all_visitdispersers/20220607/visitdispersers_20220607_filt_empty-holo4_003.list --num=1
+# python run_processstarOneImage.py --reposdir /sdf/home/d/dagoret/repos/repos_w_2022_39 --listofimages /sdf/home/d/dagoret/notebooks/AuxTelComm/notebooks_usdf/butlertools/all_visitdispersers/20220607/visitdispersers_20220607_filt_empty-holo4_003.list --num=1
 #
 
 import os
@@ -9,7 +9,7 @@ import numpy as np
 import argparse
 
 
-reposdir="/sdf/home/d/dagoret/repos/repos_w_2022_34"
+reposdir="/sdf/home/d/dagoret/repos/repos_w_2022_39"
 listofimages="/sdf/home/d/dagoret/notebooks/AuxTelComm/notebooks_usdf/butlertools/all_visitdispersers/20220607/visitdispersers_20220607_filt_empty-holo4_003.list"
 
 
@@ -47,6 +47,6 @@ seq=seqnum[num-1]
 command_line="pipetask run -b /sdf/group/rubin/repo/main -p {}/atmospec/pipelines/processStar.yaml -i LATISS/raw/all,refcats,LATISS/calib -o u/dagoret/test_auxtel_v0 -d \"exposure.day_obs={} and exposure.seq_num={} and instrument='LATISS'\" --register-dataset-types --clobber-outputs".format(REPOSDIR,dateobs,seq)
 
 print(command_line)
-#os.system(command_line)
+os.system(command_line)
 
 
