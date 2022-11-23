ad068d7fada0cfc705ceca0a0adcf77924d8edb2
Dagoret Campagne Sylvie <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2022-11-23 12:01:27 +0100

    add all scripts

---
+++ notebookccdm_gen3/mycalib/myflats/logs/2022_11_22/ControlFlats_v2_09Nov22.sh
@@ -0,0 +1,20 @@
+# /bin/sh
+
+source /cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/w_2022_39/loadLSST.bash
+setup lsst_distrib
+source /sps/lsst/groups/auxtel/softs/shared/auxteldm_gen3/repos/w_2022_39/user_setup.sh
+
+EXPOSURES='2022031700209, 2022031700211, 2022031700210, 2022031700204,2022031700208, 2022031700205, 2022031700207, 2022031700201,2022031700206'
+
+EXPOSURES_VERIFY='2022031700202, 2022031700203'
+    
+RERUN=20220317d
+BUTLER_REPO='/sps/lsst/groups/auxtel/softs/shared/auxteldm_gen3/data/butler.yaml'
+
+
+pipetask --long-log run -b $BUTLER_REPO \
+          -p $CP_VERIFY_DIR/pipelines/VerifyFlat.yaml \
+          -i LATISS/calib,LATISS/raw/all,LATISS/calib,u/dagoret/myflats.20221122/flatGen.$RERUN \
+          -o u/dagoret/myflats.20221122/verifyFlat.$RERUN \
+          -d "instrument='LATISS' AND detector=0 AND exposure IN ($EXPOSURES,$EXPOSURES_VERIFY)" --register-dataset-types \
+          >& ./flatVerify.$RERUN.log
