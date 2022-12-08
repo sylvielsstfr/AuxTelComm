2db96b3ec0c66e5a68b4621bac8749b872e6b80e
Dagoret Campagne Sylvie <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2022-12-08 23:11:52 +0100

    simulation atm

---
+++ notebookccdm_gen3/runspectractor_standalone/2022_12/slurm/slurm_processSpectra_jobarray_empty~holo4_003_20211103_rebin2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+####################################
+# SLURM options
+###################################
+#SBATCH --job-name=emptyholorebintwo
+#SBATCH --output=emptyholo_%A_%a.log
+#SBATCH --error=emptyholo_%A_%a.log
+#SBATCH --array=1-33
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=20G
+#SBATCH --partition htc
+########################################
+
+# 113 exposures
+# Print the task id.
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+
+echo "Node: $(hostname)"
+
+echo " Slurm environment variables:"
+env | grep SLURM
+
+export NEWHOME="/pbs/throng/lsst/users/dagoret"
+export AUXTELHOME="/sps/lsst/groups/auxtel"
+export AUXTELDM="${AUXTELHOME}/softs/shared/auxteldm"
+export AUXTELDM2="${AUXTELHOME}/softs/shared/auxteldm_gen3"
+#export PATH=/pbs/throng/lsst/software/desc/bin:${PATH}
+#source /pbs/throng/lsst/software/desc/common/miniconda/setup_current_python.sh 
+
+
+cd ${AUXTELHOME}/softs
+source MySetup_py39_SDC.sh 
+
+which python
+cd ${NEWHOME}/desc/AuxTelComm/notebookccdm_gen3/runspectractor_standalone/2022_12
+
+
+DAYNUM="20211103"
+FILTERDISPERSERNAME="empty~holo4_003"
+
+python processSpectra_holo-newspectractor_v3.py ${DAYNUM} ${FILTERDISPERSERNAME} ${SLURM_ARRAY_TASK_ID}
+
+
