fa42c5f9002bd375fe84e0b5f403cec274e7a63e
Sylvie Dagoret-Campagne <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2023-02-01 00:34:09 -0800

    update nb in butler tools

--- notebooks_usdf/ExtractManySpectra/CheckForSpectraction-oga.py
+++ notebooks_usdf/ExtractManySpectra/CheckForSpectraction-oga.py
@@ -54,9 +54,9 @@ registry = butler.registry
 
 #### Date & Filter & Disperser
 # path index for each month
-DATE="20230117" # with "u/dagoret/BPS_manyspectro_v7 or v11", rebin=2 or v14 with rebin=1
+#DATE="20230117" # with "u/dagoret/BPS_manyspectro_v7 or v11", rebin=2 or v14 with rebin=1
 #DATE="20230118" # with "u/dagoret/BPS_manyspectro_v8 or v10", rebin=2  or v13 with rebin=1
-#DATE="20230119" # with "u/dagoret/BPS_manyspectro_v9 or v12",rebin=2
+DATE="20230119" # with "u/dagoret/BPS_manyspectro_v9 or v12",rebin=2, or v15 and rebin=1
 
 filterdispersername = "empty~holo4_003"
 #filterdispersername = "BG40~holo4_003"
@@ -81,7 +81,8 @@ ext=""
 #my_collection = "u/dagoret/BPS_manyspectro_v11" # january 26th 2023 (2023/01/17), output with the _b, rebin=2
 #my_collection = "u/dagoret/BPS_manyspectro_v12" # january 26th 2023 (2023/01/19), output with the _b, rebin=2,
 #my_collection = "u/dagoret/BPS_manyspectro_v13" # january 31th 2023 (2023/01/18), rebin=1,
-my_collection = "u/dagoret/BPS_manyspectro_v14" # january 31th 2023 (2023/01/17), rebin=1,
+#my_collection = "u/dagoret/BPS_manyspectro_v14" # january 31th 2023 (2023/01/17), rebin=1,
+my_collection = "u/dagoret/BPS_manyspectro_v15" # january 31th 2023 (2023/01/18), rebin=1,
 
 datasetRefs = registry.queryDatasets(datasetType='spectraction', collections=my_collection, where= "instrument='LATISS'")
 
