8d978da50d80f0655880a2df990efc864b8db76d
dagoret <dagoret@lal.in2p3.fr>
2023-02-18 23:59:59 +0100

    update multifit

--- notebooks_usdf/FitAtmosphericParameters/202302/lib/libAtmosphericFit.py
+++ notebooks_usdf/FitAtmosphericParameters/202302/lib/libAtmosphericFit.py
@@ -377,8 +377,8 @@ emul = SimpleAtmEmulator(path=data_path)
  
  
     
-# Functions for the fit
-def fluxpred(params,*arg):
+# =========    Functions for the fit grey factor, pwv, ozone ========================
+def fluxpred_greypwvo3(params,*arg):
     """
     Model prediction y = f(x) where x is the array of wavelength and y the measured flux
     
@@ -419,7 +419,7 @@ def fluxpred(params,*arg):
     return fl
 
 
-def func_residuals(params,*arg):
+def func_residuals_greypwvo3(params,*arg):
     """
     This function is called by the scipy.optimize.least_squares function to compute the normalized residuals
     at each data point at each wavelength (yi-f(xi,thetaj)/sigmai
@@ -448,10 +448,10 @@ def func_residuals(params,*arg):
     the_dataerr = arg[2]    # the error on the observed fluxes array
     
      
-    alpha,oz,pwv = params   # decode the parameters
+    alpha,pwv , oz = params   # decode the parameters
     
     
-    flux_model = fluxpred(params,the_wl,the_airmass,the_sedthr)
+    flux_model = fluxpred_greypwvo3(params,the_wl,the_airmass,the_sedthr)
     
     
     residuals = (the_data - flux_model)/the_dataerr
@@ -460,6 +460,87 @@ def func_residuals(params,*arg):
     
     return residuals
 
+# =========    Functions for the fit grey factor, pwv, No ozone ========================
+def fluxpred_greypwv(params,*arg):
+    """
+    Model prediction y = f(x) where x is the array of wavelength and y the measured flux
+    
+    inputs:
+      - params : the unknown atmospheric parameters to fit (pwv,oz)
+      - *args  : additionnal data provided to compute the model : 
+                   1) the wavelength array, 
+                   3) sedxthroughput,
+                   2) the airmass
+      
+    output:
+      - the array of predicted flux values at each wavelength data point
+      
+    It computes the production of the atmospheric transparency by the SED - throughout product  
+    It makes use of the atmospheric model transparency emulated in the external global object emul 
+    which depend on atmospheric parameters to fit
+    
+    """
+    
+    global emul
+    
+    alpha,pwv = params   
+    oz=300. 
+    
+    # have to build the wavelength array as follow (I don't know why)
+    #wl = []
+    #for el in arg[0]:
+    #    wl.append(el)
+    
+   
+    wl = arg[0]
+    the_sedxthroughput = arg[2]
+    airmass = arg[1]
+        
+
+    fl = alpha*emul.GetAllTransparencies(wl ,airmass,pwv,oz,ncomp=0,flagAerosols=False)
+    
+    fl *= the_sedxthroughput
+    return fl
+
+
+def func_residuals_greypwv(params,*arg):
+    """
+    This function is called by the scipy.optimize.least_squares function to compute the normalized residuals
+    at each data point at each wavelength (yi-f(xi,thetaj)/sigmai
+    
+    input:
+      - params : the unknown atmospheric parameters to fit (pwv,oz)
+      - *args  : additionnal data provided to compute the mode : 
+                 to the model
+                   1) the wavelength array, 
+                   2) flux data : the measured fluxes
+                   3) dataerr : the error on fluxes
+                   4) the airmass
+                   5) sedxthroughput,
+                  
+    
+    return:
+      - the array of residuals for the given set of parameters required by the least_squares function 
+    """
+    
+    
+    
+    the_wl = arg[0]         # wavelength array required by the model function pred2
+    the_sedthr = arg[4]     # the product of the SED by the throughput required by the model function pred2
+    the_airmass = arg[3]    # the airmass required by the model function pred2 
+    the_data = arg[1]       # the observed fluxes array
+    the_dataerr = arg[2]    # the error on the observed fluxes array
+    
+     
+    alpha,pwv = params      # decode the parameters
+    
+    
+    flux_model = fluxpred_greypwv(params,the_wl,the_airmass,the_sedthr)
+    
+    
+    residuals = (the_data - flux_model)/the_dataerr
+    
+    return residuals
 
 
 
@@ -478,7 +559,7 @@ class FitAtmosphericParams:
 
         """
             
-        res_fit = least_squares(func_residuals, params0,bounds=([0.1,0.001,50.],[2,9.5,550.]),args=[xdata,ydata,yerrdata,airmass,sedxthroughput])
+        res_fit = least_squares(func_residuals_greypwvo3, params0,bounds=([0.1,0.001,50.],[2,9.5,550.]),args=[xdata,ydata,yerrdata,airmass,sedxthroughput])
         
         alpha_fit,pwv_fit,oz_fit = res_fit.x
     
@@ -505,7 +586,44 @@ class FitAtmosphericParams:
     def pred_greypwvo3(self,params,xdata,airmass,sedxthroughput):
         """
         """
-        flux_model = fluxpred(params,xdata,airmass,sedxthroughput)
+        flux_model = fluxpred_greypwvo3(params,xdata,airmass,sedxthroughput)
+        return flux_model
+    
+#--------
+    
+    def fit_greypwv(self,params0,xdata,ydata,yerrdata,airmass,sedxthroughput):
+        """
+        Fit a grey term, precipitable water varpor and ozone
+
+        """
+            
+        res_fit = least_squares(func_residuals_greypwv, params0,bounds=([0.1,0.001],[2,9.5]),args=[xdata,ydata,yerrdata,airmass,sedxthroughput])
+        
+        alpha_fit,pwv_fit = res_fit.x
+    
+    
+        # results fo the fit
+        popt= res_fit.x
+        J = res_fit.jac
+        pcov = np.linalg.inv(J.T.dot(J))
+        sigmas = np.sqrt(np.diagonal(pcov))
+        cost=res_fit.cost
+        residuals = res_fit.fun
+        chi2=np.sum(residuals**2)
+        ndeg = J.shape[0]- popt.shape[0]
+        chi2_per_deg = chi2/ndeg
+        pwve = sigmas[1]*np.sqrt(chi2_per_deg)
+        greye = sigmas[0]*np.sqrt(chi2_per_deg)
+        
+        fit_dict = {"chi2":chi2,"ndeg":ndeg,"chi2_per_deg":chi2_per_deg,"popt":popt,"sigmas":sigmas,"pwv_fit":pwv_fit,"grey_fit":alpha_fit,"pwve":pwve,"greye":greye}
+
+        return res_fit,fit_dict
+    
+    
+    def pred_greypwv(self,params,xdata,airmass,sedxthroughput):
+        """
+        """
+        flux_model = fluxpred_greypwv(params,xdata,airmass,sedxthroughput)
         return flux_model
         
         
