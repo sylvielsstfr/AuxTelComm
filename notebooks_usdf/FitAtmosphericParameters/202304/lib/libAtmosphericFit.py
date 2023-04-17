5924c985c56d708d86a792f58b9a0e29273724ce
dagoret <dagoret@lal.in2p3.fr>
2023-04-17 10:14:15 +0200

    show residuals

--- notebooks_usdf/FitAtmosphericParameters/202304/lib/libAtmosphericFit.py
+++ notebooks_usdf/FitAtmosphericParameters/202304/lib/libAtmosphericFit.py
@@ -343,15 +343,15 @@ g_sedxthrouput = None
 
 def func_model_greypwvo3(x, *params):
   """
-  Evaluate the fllux
+  Evaluate the flux
   
   Parameters 
-     x : the abssisse , ie the wavelength array
-    param : array of parameters to fit (amplitude, pwv, ozone)
+     x : the abscisse , ie the wavelength array
+     params : array of parameters to fit (amplitude, pwv, ozone)
 
-  Required global variables
-    g_airmass
-    g_sedxthroughput
+  Required global variables for the model
+    g_airmass :airmass
+    g_sedxthroughput : the throughput time SED
 
   Return
     the array of fluxes
@@ -364,21 +364,24 @@ def func_model_greypwvo3(x, *params):
   wl = x
   the_sedxthroughput = g_sedxthroughput
   airmass = g_airmass
+  
+  print(f"func_model_greypwvo3() get global airmass {airmass}") 
+  
   fl = alpha*emul.GetAllTransparencies(wl ,airmass,pwv,oz,ncomp=0,flagAerosols=False)
   fl *= the_sedxthroughput
   return fl
 
 def func_model_greypw(x, *params):
   """
-  Evaluate the fllux
+  Evaluate the flux
   
   Parameters 
-     x : the abssisse , ie the wavelength array
-    param : array of parameters to fit (amplitude, pwv, ozone)
+     x : the abscisse , ie the wavelength array
+     params : array of parameters to fit (amplitude, pwv)
 
   Required global variables
-    g_airmass
-    g_sedxthroughput
+    g_airmass : the airmass
+    g_sedxthroughput : the sed time throughput
 
   Return
     the array of fluxes
@@ -399,9 +402,6 @@ def func_model_greypw(x, *params):
 
 
 
-
-
-
 ########################## Fitting class with curve_fit method ###################################################### 
 # scipy.optimize.curve_fit(f, xdata, ydata, p0=None, sigma=None, 
 # absolute_sigma=False, check_finite=True, bounds=(-inf, inf), method=None, jac=None, *, full_output=False, **kwargs)
@@ -418,43 +418,62 @@ class FitAtmosphericParamsCov:
 
     def fit_greypwvo3(self,params0,xdata,ydata,covdata,airmass,sedxthroughput):
         """
-        Fit a grey term, precipitable water varpor and ozone
+        Fit a grey term, precipitable water varpor and ozone.
+        It provides the covariance matrix (not the error vector)
 
         """
-        # first pass the global parameters
+        # first pass the global parameters to the func_model_grewpwvo3 function
         global g_airmass 
         g_airmass = airmass
         global g_sedxthroughput 
         g_sedxthroughput = sedxthroughput
-
-            
-        res_fit = curve_fit(func_model_greypwvo3, xdata=xdata, ydata=ydata, p0=params0,sigma = covdata,bounds=([0.1,0.001,50.],[2,9.5,550.]), full_output=True)
-        
-        popt,pcov,fit_dict = res_fit
-        
+ 
+        res_fit = curve_fit(func_model_greypwvo3, xdata=xdata, ydata=ydata, p0=params0,sigma = covdata,bounds=([0.1,0.001,50.],[2,9.5,550.]), full_output=True)     
     
+        popt = res_fit[0]
+        pcov = res_fit[1]
+        cf_dict = res_fit[2]
     
-        # results fo the fit
-        popt= res_fit.x
-        J = res_fit.jac
-        pcov = np.linalg.inv(J.T.dot(J))
+        # compute the errors on fitted parameters
         sigmas = np.sqrt(np.diagonal(pcov))
-        cost=res_fit.cost
-        residuals = res_fit.fun
-        chi2=np.sum(residuals**2)
-        ndeg = J.shape[0]- popt.shape[0]
-        chi2_per_deg = chi2/ndeg
-        pwve = sigmas[1]*np.sqrt(chi2_per_deg)
-        oze =sigmas[2]*np.sqrt(chi2_per_deg)
-        greye = sigmas[0]*np.sqrt(chi2_per_deg)
+        # compute the normalized residuals  
+        normresiduals=cf_dict['fvec']
+        # compute the chi2 from the 
+        chi2= np.sum(normresiduals**2)
+        ndf = len(normresiduals)-len(popt)
+        chi2_per_ndf = chi2/ndf
         
-        fit_dict = {"chi2":chi2,"ndeg":ndeg,"chi2_per_deg":chi2_per_deg,"popt":popt,"sigmas":sigmas,"pwv_fit":pwv_fit,"oz_fit":oz_fit,"grey_fit":alpha_fit,"pwve":pwve,"oze":oze,"greye":greye}
+        #another method to calculate residuals
+        pred = func_model_greypwvo3(xdata,*popt)
+        residuals = ydata-pred
+        r=residuals
+        chi2 = r.T @ np.linalg.inv(covdata) @ r
+        ndf = len(residuals)-len(popt)
+        chi2_per_ndf = chi2/ndf
+      
+        pwv_fit = popt[1]
+        oz_fit = popt[2]
+        alpha_fit = popt[0]
+        
+        pwve = sigmas[1]*np.sqrt(chi2_per_ndf)
+        oze =sigmas[2]*np.sqrt(chi2_per_ndf)
+        greye = sigmas[0]*np.sqrt(chi2_per_ndf)
+        
+        fit_dict = {"chi2":chi2,"ndeg":ndf,"chi2_per_deg":chi2_per_ndf,"popt":popt,"sigmas":sigmas,"pwv_fit":pwv_fit,"oz_fit":oz_fit,"grey_fit":alpha_fit,"pwve":pwve,"oze":oze,"greye":greye}
 
-        return res_fit,fit_dict
+        return popt,sigmas,fit_dict
     
     
     def pred_greypwvo3(self,params,xdata,airmass,sedxthroughput):
         """
+        Parameters
+          params parameters of the fit , ie alpha, pwv,oz
+          xdata : wavelength array
+          airmass : airmass
+          sedxthroughput : product of the SED by the throughput
+          
+        Return
+          the flux array for the xdata array
         """
         # first pass the global parameters
         global g_airmass 
@@ -469,11 +488,11 @@ class FitAtmosphericParamsCov:
     
     def fit_greypwv(self,params0,xdata,ydata,covdata,airmass,sedxthroughput):
         """
-        Fit a grey term, precipitable water varpor but No  ozone
+        Fit a grey term, precipitable water varpor but No  ozone.
+        This case applies when fittingg with the red filter.
+        It is assumed an ozone value of 300 DB
 
         """
-
-
         # first pass the global parameters
         global g_airmass 
         g_airmass = airmass
@@ -482,25 +501,39 @@ class FitAtmosphericParamsCov:
             
         res_fit = curve_fit(func_model_greypw,  xdata=xdata, ydata=ydata, p0=params0,sigma = covdata,bounds=([0.1,0.001],[2,9.5]))
         
-        alpha_fit,pwv_fit = res_fit.x
+        popt = res_fit[0]
+        pcov = res_fit[1]
+        cf_dict = res_fit[2]
     
-    
-        # results fo the fit
-        popt= res_fit.x
-        J = res_fit.jac
-        pcov = np.linalg.inv(J.T.dot(J))
+        # compute the errors on fitted parameters
         sigmas = np.sqrt(np.diagonal(pcov))
-        cost=res_fit.cost
-        residuals = res_fit.fun
-        chi2=np.sum(residuals**2)
-        ndeg = J.shape[0]- popt.shape[0]
-        chi2_per_deg = chi2/ndeg
-        pwve = sigmas[1]*np.sqrt(chi2_per_deg)
-        greye = sigmas[0]*np.sqrt(chi2_per_deg)
+        # compute the normalized residuals  
+        normresiduals=cf_dict['fvec']
+        # compute the chi2 from the 
+        chi2= np.sum(normresiduals**2)
+        ndf = len(normresiduals)-len(popt)
+        chi2_per_ndf = chi2/ndf
         
-        fit_dict = {"chi2":chi2,"ndeg":ndeg,"chi2_per_deg":chi2_per_deg,"popt":popt,"sigmas":sigmas,"pwv_fit":pwv_fit,"grey_fit":alpha_fit,"pwve":pwve,"greye":greye}
+        #another method to calculate residuals
+        pred = func_model_greypwvo3(xdata,*popt)
+        residuals = ydata-pred
+        r=residuals
+        chi2 = r.T @ np.linalg.inv(covdata) @ r
+        ndf = len(residuals)-len(popt)
+        chi2_per_ndf = chi2/ndf
+      
+        pwv_fit = popt[1]
+        alpha_fit = popt[0]
+        
+        
+        pwve = sigmas[1]*np.sqrt(chi2_per_ndf)
+        greye = sigmas[0]*np.sqrt(chi2_per_ndf)
+        
+        fit_dict = {"chi2":chi2,"ndeg":ndf,"chi2_per_deg":chi2_per_ndf,"popt":popt,"sigmas":sigmas,"pwv_fit":pwv_fit,"grey_fit":alpha_fit,"pwve":pwve,"greye":greye}
 
-        return res_fit,fit_dict
+        return popt,sigmas,fit_dict
+        
+        
     
     
     def pred_greypwv(self,params,xdata,airmass,sedxthroughput):
