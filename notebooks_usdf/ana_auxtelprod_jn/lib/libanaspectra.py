933a0adaadfb2f94cbaa26b25290ef160eed1fec
Sylvie Dagoret-Campagne <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2024-01-14 07:01:47 -0800

    update

---
+++ notebooks_usdf/ana_auxtelprod_jn/lib/libanaspectra.py
@@ -0,0 +1,140 @@
+import lsst.daf.butler as dafButler
+import numpy as np
+import matplotlib.pyplot as plt
+
+import matplotlib.colors as mcolors
+import matplotlib.cm as cm
+
+
+def select_files(butler,collection, where):
+    """
+    Select all records according the where clause
+    """
+    # datasetRefs = registry.queryDatasets(datasetType='spectractorSpectrum', collections=my_collection, where=where)
+    #records = list(butler.registry.queryDimensionRecords('exposure', where=where))
+    records = list(butler.registry.queryDimensionRecords('exposure', datasets='spectractorSpectrum', where=where,  collections=collection))
+    records = sorted(records, key=lambda x: x.id, reverse=False)
+    return records
+
+def filter_data(butler,collection,dateobs,records, sigma_clip=3,remove_withfilters = True):  # pragma: no cover
+    """
+    Spectrum reconstruction Quality Selection
+    """
+    from scipy.stats import median_abs_deviation
+    D = []
+    chi2 = []
+    dx = []
+    amplitude = []
+    regs = []
+    times = []
+    specs = []
+    alpha_0_2 = []
+    #parameters.VERBOSE = False
+    #parameters.DEBUG = False
+    spec_flagfilter = []
+    for i, r in enumerate(records):
+        times.append(r.day_obs)
+        spec = butler.get('spectractorSpectrum', visit=r.id, collections=collection, detector=0, instrument='LATISS')
+        spec.dataId = r.id
+        spec.physical_filter = r.physical_filter
+
+        if spec.x0[0] > 500: 
+            continue
+            
+        flag_filter = spec.physical_filter == 'empty~holo4_003'
+        spec_flagfilter.append(flag_filter)
+        
+        D.append(spec.header["D2CCD"])
+        dx.append(spec.header["PIXSHIFT"])
+        regs.append(np.log10(spec.header["PSF_REG"]))
+        # what is amplitude[300:]
+        amplitude.append(np.sum(np.abs(spec.data[300:])))
+        # if "CHI2_FIT" in header:
+        chi2.append(spec.header["CHI2_FIT"])
+        specs.append(spec)
+        p = butler.get('spectrumForwardModelFitParameters', visit=r.id, collections=collection, detector=0, instrument='LATISS')
+        alpha_0_2.append(p.values[p.get_index("alpha_0_2")])
+        #except:
+        #    new_file_names.remove(name)
+        #    print(f"fail to open {name}. len(file_names)={len(new_file_names)}")
+    params = {'D2CCD': np.array(D),
+              'dx': np.array(dx),
+              'regs': np.array(regs),
+              'chi2': np.array(chi2),
+              'amplitude': np.array(amplitude),
+              'alpha_0_2': np.array(alpha_0_2)
+             }
+    k = np.arange(len(D))
+    spec_flagfilter = np.array(spec_flagfilter)
+    #spec_intfilter = np.array(list(map(lambda x: 1 if x else 0, spec_flagfilter)))
+    
+    # array of filters , one per file
+    if remove_withfilters:     
+        filter_indices = spec_flagfilter 
+    else:
+        filter_indices = np.ones_like(k, dtype=bool)    
+    for par in params.keys():
+        if par in ['amplitude']: #, 'alpha_0_2']:
+            continue
+        filter_indices *= np.logical_and(params[par] > np.median(params[par]) - sigma_clip * median_abs_deviation(params[par]),
+                                         params[par] < np.median(params[par]) + sigma_clip * median_abs_deviation(params[par]))
+    for par in params.keys():
+        fig = plt.figure(figsize=(8,4))
+        plt.plot(k, params[par])
+        plt.plot(k[filter_indices], params[par][filter_indices], "ko")
+        plt.grid()
+        plt.title(par)
+
+        suptitle = f"Observations : {dateobs} \n collection = {collection}"
+        plt.suptitle(suptitle,fontsize=10,y=1.00)
+        plt.show()
+    return [s for i,s in enumerate(specs) if filter_indices[i]]
+
+
+def plot_spectra(spectra, colorparams,collection,dateobs):
+    """
+    plot spectra
+    """
+    
+    colormap = cm.Reds
+    #colormap = cm.jet 
+
+    normalize = mcolors.Normalize(vmin=np.min(colorparams), vmax=np.max(colorparams))
+
+    all_target_names = [] 
+
+    fig  = plt.figure(figsize=(11,6))
+    for spec in spectra:
+        target_name = spec.target.label
+        if target_name in all_target_names:
+            plt.plot(spec.lambdas, spec.data, color = colormap(normalize(spec.airmass)))
+        else:
+            plt.plot(spec.lambdas, spec.data, color = colormap(normalize(spec.airmass)),label=target_name)
+            all_target_names.append(target_name)
+            
+    plt.grid()
+    plt.xlabel("$\lambda$ [nm]")
+    plt.ylabel(f"Flux [{spec.units}]")
+    plt.legend()
+    
+    # Colorbar setup
+    s_map = cm.ScalarMappable(norm=normalize, cmap=colormap)
+    s_map.set_array(colorparams)
+
+    # If color parameters is a linspace, we can set boundaries in this way
+    halfdist = (colorparams[1] - colorparams[0])/2.0
+    boundaries = np.linspace(colorparams[0] - halfdist, colorparams[-1] + halfdist, len(colorparams) + 1)
+
+    # Use this to emphasize the discrete color values
+    cbar = fig.colorbar(s_map) #, spacing='proportional', ticks=colorparams, boundaries=boundaries, format='%2.2g') # format='%2i' for integer
+
+    # Use this to show a continuous colorbar
+    #cbar = fig.colorbar(s_map, spacing='proportional', ticks=colorparams, format='%2i')
+    cbar.set_label("Airmass $z$")
+    title = f"Observations : {dateobs}"
+    suptitle = f"collection = {collection}"
+    plt.title(title)
+    plt.suptitle(suptitle,fontsize=10)
+    plt.show()
+    return fig
+
